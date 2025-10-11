3853bb882db0abd2d092898e2392f3bd8eb3367c
Sean Best <bests3@farmingdale.edu>
2025-10-11 12:15:40 -0400

    Initial commit Color Chooser

---
+++ src/main/java/org/example/colorchooser/Launcher.java
@@ -0,0 +1,9 @@
+package org.example.colorchooser;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(ColorChooser.class, args);
+    }
+}

d644bfe2a39c88d34b5a5672cb491c6cec937886
Sean Best <bests3@farmingdale.edu>
2025-10-14 19:17:05 -0400

    Added listener to textfield

--- src/main/java/org/example/colorchooser/ColorChooserController.java
+++ src/main/java/org/example/colorchooser/ColorChooserController.java
@@ -8,7 +8,6 @@ import javafx.scene.control.TextField;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
-
 public class ColorChooserController {
     @FXML private Slider redSlider;
     @FXML private Slider greenSlider;
@@ -25,52 +24,133 @@ public class ColorChooserController {
     private int blue = 0;
     private double alpha = 1.0;
 
+    private boolean updating = false; // Prevent event loops
+
     public void initialize() {
-        redTextField.textProperty().bind(
-                redSlider.valueProperty().asString("%.0f"));
-        greenTextField.textProperty().bind(
-                greenSlider.valueProperty().asString("%.0f"));
-        blueTextField.textProperty().bind(
-                blueSlider.valueProperty().asString("%.0f"));
-        alphaTextField.textProperty().bind(
-                alphaSlider.valueProperty().asString("%.0f"));
+        // --- Slider to TextField + Color Sync ---
+        redSlider.valueProperty().addListener((obs, oldVal, newVal) -> {
+            if (!updating) {
+                updating = true;
+                red = newVal.intValue();
+                redTextField.setText(String.valueOf(red));
+                updateColor();
+                updating = false;
+            }
+        });
+        greenSlider.valueProperty().addListener((obs, oldVal, newVal) -> {
+            if (!updating) {
+                updating = true;
+                green = newVal.intValue();
+                greenTextField.setText(String.valueOf(green));
+                updateColor();
+                updating = false;
+            }
+        });
+        blueSlider.valueProperty().addListener((obs, oldVal, newVal) -> {
+            if (!updating) {
+                updating = true;
+                blue = newVal.intValue();
+                blueTextField.setText(String.valueOf(blue));
+                updateColor();
+                updating = false;
+            }
+        });
+        alphaSlider.valueProperty().addListener((obs, oldVal, newVal) -> {
+            if (!updating) {
+                updating = true;
+                alpha = newVal.doubleValue();
+                alphaTextField.setText(String.format("%.2f", alpha));
+                updateColor();
+                updating = false;
+            }
+        });
 
-        redSlider.valueProperty().addListener(
-                new ChangeListener<Number>() {
-                    @Override
-                    public void changed(ObservableValue<? extends Number> ov, Number oldValue, Number newValue) {
-                        red = newValue.intValue();
-                        colorRectangle.setFill(Color.rgb(red, green, blue, alpha));
-                    }
+        // --- TextField to Slider + Color Sync ---
+        redTextField.textProperty().addListener((obs, oldVal, newVal) -> {
+            if (!updating) {
+                try {
+                    updating = true;
+                    int value = Integer.parseInt(newVal);
+                    if (value < 0) value = 0;
+                    if (value > 255) value = 255;
+                    red = value;
+                    redSlider.setValue(red);
+                    updateColor();
+                } catch (NumberFormatException e) {
+                    // ignore
+                } finally {
+                    updating = false;
                 }
-        );
-        greenSlider.valueProperty().addListener(
-                new ChangeListener<Number>() {
-                    @Override
-                    public void changed(ObservableValue<? extends Number> ov, Number oldValue, Number newValue) {
-                        green = newValue.intValue();
-                        colorRectangle.setFill(Color.rgb(red, green, blue, alpha));
-                    }
+            }
+        });
+        greenTextField.textProperty().addListener((obs, oldVal, newVal) -> {
+            if (!updating) {
+                try {
+                    updating = true;
+                    int value = Integer.parseInt(newVal);
+                    if (value < 0) value = 0;
+                    if (value > 255) value = 255;
+                    green = value;
+                    greenSlider.setValue(green);
+                    updateColor();
+                } catch (NumberFormatException e) {
+                    // ignore
+                } finally {
+                    updating = false;
                 }
-        );
-        blueSlider.valueProperty().addListener(
-                new ChangeListener<Number>() {
-                    @Override
-                    public void changed(ObservableValue<? extends Number> ov, Number oldValue, Number newValue) {
-                        blue = newValue.intValue();
-                        colorRectangle.setFill(Color.rgb(red, green, blue, alpha));
-                    }
+            }
+        });
+        blueTextField.textProperty().addListener((obs, oldVal, newVal) -> {
+            if (!updating) {
+                try {
+                    updating = true;
+                    int value = Integer.parseInt(newVal);
+                    if (value < 0) value = 0;
+                    if (value > 255) value = 255;
+                    blue = value;
+                    blueSlider.setValue(blue);
+                    updateColor();
+                } catch (NumberFormatException e) {
+                    // ignore
+                } finally {
+                    updating = false;
                 }
-        );
-        alphaSlider.valueProperty().addListener(
-                new ChangeListener<Number>() {
-                    @Override
-                    public void changed(ObservableValue<? extends Number> ov, Number oldValue, Number newValue) {
-                        alpha = newValue.doubleValue();
-                        colorRectangle.setFill(Color.rgb(red, green, blue, alpha));
-                    }
+            }
+        });
+        alphaTextField.textProperty().addListener((obs, oldVal, newVal) -> {
+            if (!updating) {
+                try {
+                    updating = true;
+                    double value = Double.parseDouble(newVal);
+                    if (value < 0.0) value = 0.0;
+                    if (value > 1.0) value = 1.0;
+                    alpha = value;
+                    alphaSlider.setValue(alpha);
+                    updateColor();
+                } catch (NumberFormatException e) {
+                    // ignore
+                } finally {
+                    updating = false;
                 }
-        );
+            }
+        });
 
+        // Set initial values
+        redSlider.setValue(red);
+        greenSlider.setValue(green);
+        blueSlider.setValue(blue);
+        alphaSlider.setValue(alpha);
+
+        // Initialize text fields
+        redTextField.setText(String.valueOf(red));
+        greenTextField.setText(String.valueOf(green));
+        blueTextField.setText(String.valueOf(blue));
+        alphaTextField.setText(String.format("%.2f", alpha));
+
+        updateColor();
+    }
+
+    private void updateColor() {
+        colorRectangle.setFill(Color.rgb(red, green, blue, alpha));
     }
 }
